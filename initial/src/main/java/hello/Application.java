7ca6657569d986add750e5990ecf37da3d9df8e1
two8g <ybxnice@gmail.com>
2015-11-01 17:21:49 +0800

    greeting RESTFul web server

---
+++ initial/src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 易斌鑫 on 15-11-1.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
